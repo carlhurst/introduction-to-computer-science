7c979f1047acf5662e16a545dee5c134bb853780
carlhurst <carl.hurst@outlook.com>
2017-08-30 18:51:33 +0400

    Addition of the simulation for uploading

--- Chapter-9/Simulation.py
+++ Chapter-9/Simulation.py
@@ -5,47 +5,63 @@ would be expected given the players ability
 A game consists of 15 points and each point can only me won when a player wins during his serve if he looses on his serve then serve passes to the other people."""
 
 def Intro():
-    print("""This programme is designed to simulate racket ball games and determine whether the person is loosing,
-     more than expected given their level of ability.""")
+    print("This programme is designed to simulate racket ball games and determine whether the person is loosing " +
+          "more than expected given their level of ability.")
 
 def Input():
+    playerA = eval(input("Please input player A ability level based in a percentage form (.0): "))
+    playerB = eval(input("Please input player B ability level based in a percentage form (.0): "))
+    N = eval(input("How many games would you like to simulate: "))
 
-    playerA = eval(input("Please input player A ability level based in a percentage form ( .0"))
-    playerB = eval(input("Please input player A ability level based in a percentage form ( .0"))
-
-    return playerA, playerB
+    return playerA, playerB, N
 
 def simngames(n, probA, ProbB):
+    # Scores counter for the for loop
 
+    A_Score = 0
+
+    B_Score = 0
+
+    # for loop for the amount of games which is to be run
     for i in range(n):
-        simgame()
+        Winner = simgame(probA, ProbB)
+        print("Cycle: " + str(i))
+        if Winner == 'A':
+            A_Score += 1
+        else:
+            B_Score += 1
+
+    return A_Score, B_Score
 
 
 
 def simgame(prob_a, prob_b):
+    serv = 'A'
 
     score_a = 0
     score_b = 0
-
-    serv = 'A'
 
     while (True):
         # Serv A
         if r.random() < prob_a and serv == 'A':
             score_a += 1
-            print("Serve A win")
+            # print("Serve A win")
         else:
             serv = 'B'
-            print("A Lost Serve")
+            # print("A Lost Serve")
         # Serv B
         if r.random() < prob_b and serv == 'B':
             score_b +=1
-            print("Serve B win")
+            # print("Serve B win")
         else:
             serv = "A"
-            print("B Lost Serve")
+            # print("B Lost Serve")
 
         if score_a == 15 or score_b ==15:
+            if score_a == 15:
+                return 'A'
+            else:
+                return 'B'
             break
 
 
@@ -56,9 +72,12 @@ def GameOver():
 def main():
     Intro()
 
-    playA, playB = Input()
+    playA, playB, N = Input()
 
-    simgame(playA, playB)
+    A, B = simngames(N, playA, playB)
+
+    print(A)
+    print(B)
 
 
 if __name__ == '__main__':
