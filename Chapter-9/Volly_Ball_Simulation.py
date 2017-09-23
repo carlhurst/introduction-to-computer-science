56c7f33ae8c04d57b463dee8de4e98096e10b536
carlhurst <carl.hurst@outlook.com>
2017-09-23 13:01:46 +0100

    Formating
    Changes to the formating for the Simulation

--- Chapter-9/Volly_Ball_Simulation.py
+++ Chapter-9/Volly_Ball_Simulation.py
@@ -12,44 +12,44 @@ def Greeting():
 
     team_b = eval(input("\n Please input the level of ability of both the teams in the following format (0.1): "))
 
-    Number_of_Games = eval(input("\n Please enter the number of the amount of games you wish to simulate."))
+    number_of_games = eval(input("\n Please enter the number of the amount of games you wish to simulate."))
 
-    return team_a, team_b,  Number_of_Games
+    return team_a, team_b,  number_of_games
 
 
-def GameTraditional(ability_a, ability_b):
+def game_traditional(ability_a, ability_b):
 
     serv = 'A'
 
-    scoreA = 0
+    score_a = 0
 
-    scoreB = 0
+    score_b = 0
 
     while True:
 
         if (r.random() <= ability_a) and (serv == 'A'):
 
-            scoreA += 1
+            score_a += 1
 
         else:
             serv = 'B'
 
         if (r.random() <= ability_b) and (serv == 'B'):
 
-            scoreB += 1
+            score_b += 1
         else:
             serv = 'A'
 
-        if scoreA == 15:
+        if score_a == 15:
 
             return 'A'
 
-        elif scoreB == 15:
+        elif score_b == 15:
 
             return 'B'
 
 
-def GameNew(Ability_a, Ability_b):
+def game_new(ability_a, ability_b):
     scoreA = 0
 
     scoreB = 0
@@ -58,30 +58,30 @@ def GameNew(Ability_a, Ability_b):
 
     while True:
 
-        if Serv == 'A':
+        if serv == 'A':
 
-            if r.random() <= Ability_a:
-
-                scoreA += 1
-
-                Serv = 'B'
-
-            elif r.random() <= Ability_b:
-
-                scoreB += 1
-                Serv = 'B'
-
-        if Serv == 'B':
-
-            if r.random() <= Ability_b:
+            if r.random() <= ability_a:
 
                 scoreA += 1
-                Serv = 'A'
 
-            elif r.random() <= Ability_a:
+                serv = 'B'
+
+            elif r.random() <= ability_b:
 
                 scoreB += 1
-                Serv = 'A'
+                serv = 'B'
+
+        if serv == 'B':
+
+            if r.random() <= ability_b:
+
+                scoreA += 1
+                serv = 'A'
+
+            elif r.random() <= ability_a:
+
+                scoreB += 1
+                serv = 'A'
 
         if scoreA == 15:
             return 'A'
@@ -89,53 +89,34 @@ def GameNew(Ability_a, Ability_b):
             return 'B'
 
 
+def sim_n_games(ability_a, ability_b, n):
 
-def SimNGames(ability_a, ability_b, n):
-    Traditional_A_Team_Score = 0
+    traditional_score_a = 0
 
-    Traditional_B_Team_Score = 0
-
-    New_A_Team_Score = 0
-
-    New_B_Team_Score = 0
-
-
+    traditional_score_b = 0
 
     for i in range(n):
+        result = game_traditional(ability_a, ability_b)
 
-        a = GameTraditional(ability_a, ability_b)
+        print(i)
 
-        # print(i)
+        if result == 'A':
+            traditional_score_a += 1
 
-        if a == 'A':
-            Traditional_A_Team_Score += 1
-            # print("Team A Win")
+        elif result == 'B':
+            traditional_score_b += 1
 
-        elif a == 'B':
-            Traditional_B_Team_Score += 1
-            # print("Team B Win")
-
-        b = GameNew(ability_a, ability_b)
-
-        # print(i)
-
-        if b == 'A':
-            New_A_Team_Score += 1
-
-        elif b == 'B':
-            New_B_Team_Score += 1
-
-    return Traditional_A_Team_Score, Traditional_B_Team_Score, New_A_Team_Score, New_B_Team_Score
+    return traditional_score_a, traditional_score_b
 
 
 def main():
-    Team_A, Team_B, Number_of_Games = Greeting()
+    team_a, team_b, number_of_games = greeting()
 
-    Team_A_Score, Team_B_score, New_A_Score, New_B_Score = SimNGames(Team_A, Team_B, Number_of_Games)
+    team_a_score, team_b_score = sim_n_games(team_a, team_b, number_of_games)
 
-    print("The Traditional style of match Team A Won \t {} \t Vs. \t Team B {}".format(Team_A_Score, Team_B_score))
+    print("The Traditional style of match Team A Won {} ({:1}%) Vs. Team B {}".format(team_a_score, (team_a_score / (team_a_score + team_b_score)) * 100,  team_b_score))
 
-    print("\nThe new style of match Team A Won \t {} \t Vs. \t Team B {}".format(New_A_Score, New_B_Score))
 
 if __name__ == '__main__':
+
     main()
