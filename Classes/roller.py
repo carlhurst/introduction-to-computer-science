1bc36b6c4f973cfb482191825dd34e2682139c1b
carlhurst <carl.hurst@outlook.com>
2017-10-10 14:03:58 +0400

    Creation of the DieView Class

---
+++ Classes/roller.py
@@ -0,0 +1,11 @@
+# Roller.py
+# Graphics programme to roll a pair of dice. uses custom widgets
+# Button and DieView
+
+from random import random
+from graphics import GraphWin, Point
+from ButtonClass import Button
+from DieView import DieView
+
+
+def main():
